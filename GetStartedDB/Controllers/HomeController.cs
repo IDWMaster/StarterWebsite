35c46258d72ebf991356c9fe6b2a3ea34326d7e3
Brian Bosak <brielcccc@gmail.com>
2016-05-16 10:00:35 -0500

    Added redirect thingy

--- GetStartedDB/Controllers/HomeController.cs
+++ GetStartedDB/Controllers/HomeController.cs
@@ -3,14 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using System.Threading.Tasks;
+using System.Security.Cryptography;
 namespace GetStartedDB.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : SController
     {
-        public ActionResult Index()
+        public ActionResult OpenReg()
         {
-            return View();
+
+            System.Diagnostics.Process.Start(Request.PhysicalApplicationPath + "\\Models\\IDWReg.cs");
+            return Content("OK");
+        }
+        public ActionResult setupKey()
+        {
+            string cb = "";
+            using (RSACryptoServiceProvider msp = new RSACryptoServiceProvider())
+            {
+                msp.KeySize = 8192;
+                byte[] priv = msp.ExportCspBlob(true);
+                System.IO.File.WriteAllBytes(IDWReg.key_path, priv);
+                byte[] pub = msp.ExportCspBlob(false);
+                System.IO.File.WriteAllBytes(IDWReg.key_path + "_pub", pub);
+                //Upload key to website
+                cb = Convert.ToBase64String(pub);
+            }
+                return View(new GetStartedApplication.Models.KeySetup() { pubkey =cb  });
+        }
+        public async Task<ActionResult> Index()
+        {
+            try
+            {
+                await DBConnect();
+                return View();
+            }catch(Exception er)
+            {
+                return RedirectToAction("setupKey");
+            }
         }
 
         public ActionResult About()
---
+++ GetStartedDB/Controllers/SController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+using IDWDBClient;
+namespace GetStartedDB.Controllers
+{
+    public class SController:Controller
+    {
+        public SController()
+        {
+            
+        }
+        public async Task<DatabaseClient> DBConnect()
+        {
+            var retval = new DatabaseClient(IDWReg.Endpoint,IDWReg.ServerKey,System.IO.File.ReadAllBytes(IDWReg.key_path));
+            try
+            {
+                await retval.ConnectAsync();
+            }catch(Exception er)
+            {
+                retval.Dispose();
+                throw er;
+            }
+            return retval;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+    }
+}
