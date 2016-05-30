7f07f3f5f4d057dd48495b80b5591f3e5bb3a476
Brian Bosak <brielcccc@gmail.com>
2016-05-30 14:35:46 -0500

    Misc. changes

--- GetStartedDB/Global.asax.cs
+++ GetStartedDB/Global.asax.cs
@@ -18,22 +18,8 @@ namespace GetStartedDB
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
         }
 
-        private System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
-        {
-            string fileName = args.Name.Substring(0, args.Name.IndexOf(","));
-            if (Environment.Is64BitProcess)
-            {
-                var ably = Assembly.LoadFile(HttpContext.Current.Request.PhysicalApplicationPath + "bin/x64/" + fileName + ".dll");
-                return ably;
-            }
-            else
-            {
-                var ably = Assembly.LoadFile(HttpContext.Current.Request.PhysicalApplicationPath + "bin/x86/" + fileName + ".dll");
-                return ably;
-            }
-        }
+       
     }
 }
